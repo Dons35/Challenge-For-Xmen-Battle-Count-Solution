9da8d8d9feca97e4a849168bbd7f9176340d8c97
DESKTOP-ABAISK4\Donovan <donss35@gmail.com>
2018-01-25 17:17:33 -0700

    Add finished challenge

---
+++ CSASP_026Challenge_Code/CS-ASP_026-Challenge_Code/ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers.Max())
+                {
+                    string maxbattles = string.Format("{0} has the most battles.<br />", names[i]);
+                    result += maxbattles;
+                }
+
+                if (numbers[i] == numbers.Min())
+                {
+                    string minbattles = string.Format("{0} has the least battles.", names[i]);
+                    result += minbattles;
+                }
+            }
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
